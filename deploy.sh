b5f7f4b65666be2fec012774d32dbbe169e7c8a1
Jenkins <jenkins@example.com>
2019-06-24 15:35:16 +0300

    script deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 rm -rf /home/jenkins/html && mkdir /home/jenkins/html && cd /home/jenkins/
 git clone git@github.com:Gavrysh/school-php.git /home/jenkins/html/
 cd ~ && tar --exclude=\'.git\' --exclude=\'Jenkinsfile\' -zcvf deploy.tar.gz html/
